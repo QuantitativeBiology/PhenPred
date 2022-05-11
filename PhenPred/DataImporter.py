b0c45c34cafc29c5970f832a47c412db94938db6
eg14 <emanuelvgoncalves@gmail.com>
2022-05-11 10:37:36 +0100

    crispr output prediction

--- PhenPred/DataImporter.py
+++ PhenPred/DataImporter.py
@@ -2,6 +2,7 @@
 # Copyright (C) 2022 Emanuel Goncalves
 
 import pandas as pd
+from PhenPred.Utils import scale
 
 
 class DataImporter:
@@ -24,21 +25,55 @@ class DataImporter:
 		elif dataset_name.lower() == "tissue":
 			return self.read_tissue_types()
 
+		elif dataset_name.lower() == "methylation":
+			return self.read_methylation()
+
+		elif dataset_name.lower() == "transcriptomics":
+			return self.read_transcriptomics()
+
+		elif dataset_name.lower() == "genomics":
+			return self.read_genomics()
+
+		elif dataset_name.lower() == "essentiality":
+			return self.read_essentiality()
+
 		else:
 			raise Exception(f"Dataset {dataset_name}, not suported.")
 
 	def read_proteomics(self):
-		df = pd.read_csv(f"{self.data_dir}/proteomics.csv", index_col=0).T
-
-		return df
+		return pd.read_csv(f"{self.data_dir}/proteomics.csv", index_col=0).T
 
 	def read_metabolomics(self):
-		df = pd.read_csv(f"{self.data_dir}/metabolomics.csv", index_col=0).T
+		return pd.read_csv(f"{self.data_dir}/metabolomics.csv", index_col=0).T
+
+	def read_drug_response(self, only_gdsc2=True):
+		df = pd.read_csv(f"{self.data_dir}/drugresponse.csv", index_col=0).T
+
+		if only_gdsc2:
+			df = df[[c for c in df if c.split(";")[2] == "GDSC2"]]
 
 		return df
-
-	def read_drug_response(self):
-		return pd.read_csv(f"{self.data_dir}/drugresponse.csv", index_col=0).T
 
 	def read_tissue_types(self):
 		return pd.get_dummies(pd.read_csv(f"{self.data_dir}/samplesheet.csv", index_col=0)["tissue"])
+
+	def read_methylation(self):
+		return pd.read_csv(f"{self.data_dir}/methylation.csv", index_col=0).T
+
+	def read_transcriptomics(self):
+		return pd.read_csv(f"{self.data_dir}/transcriptomics.csv", index_col=0).T
+
+	def read_genomics(self):
+		return pd.read_csv(f"{self.data_dir}/genomics.csv", index_col=0).T
+
+	def read_essentiality(self, only_context_fitness_genes=True):
+		df = pd.read_csv(f"{self.data_dir}/crisprcas9.csv", index_col=0).dropna(axis=1)
+		df = scale(df)
+		df = df.T
+
+		if only_context_fitness_genes:
+			cf_genes = (df < -0.5).sum()
+			cf_genes = cf_genes.loc[(cf_genes < df.shape[0] * 0.5) & (cf_genes >= 10)]
+			df = df[cf_genes.index]
+
+		return df
--- PhenPred/MLPhenotype.sh
+++ PhenPred/MLPhenotype.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
+python3 PhenPred/MLPhenotype.py config/config_rf.yml methylation
+python3 PhenPred/MLPhenotype.py config/config_rf.yml genomics
+python3 PhenPred/MLPhenotype.py config/config_rf.yml transcriptomics
+python3 PhenPred/MLPhenotype.py config/config_rf.yml proteomics
 python3 PhenPred/MLPhenotype.py config/config_rf.yml metabolomics
 python3 PhenPred/MLPhenotype.py config/config_rf.yml tissue
+python3 PhenPred/MLPhenotypePlot.py
