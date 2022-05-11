4cefbdd0a728cac3d527bd9e9e4a0ebfcb4b95a4
eg14 <emanuelvgoncalves@gmail.com>
2022-05-11 11:48:04 +0100

    drug with tissue

--- PhenPred/MLPhenotype.sh
+++ PhenPred/MLPhenotype.sh
@@ -4,5 +4,5 @@ python3 PhenPred/MLPhenotype.py config/config_rf.yml genomics
 python3 PhenPred/MLPhenotype.py config/config_rf.yml transcriptomics
 python3 PhenPred/MLPhenotype.py config/config_rf.yml proteomics
 python3 PhenPred/MLPhenotype.py config/config_rf.yml metabolomics
-python3 PhenPred/MLPhenotype.py config/config_rf.yml tissue
+# python3 PhenPred/MLPhenotype.py config/config_rf.yml tissue
 python3 PhenPred/MLPhenotypePlot.py
