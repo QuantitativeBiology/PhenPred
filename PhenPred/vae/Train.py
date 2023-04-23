6835542cdf162a2401b69df0abc3c337744ce5fc
EmanuelGoncalves <emanuelvgoncalves@gmail.com>
2023-04-23 22:38:26 +0100

    contextualized attention

--- PhenPred/vae/Train.py
+++ PhenPred/vae/Train.py
@@ -40,14 +40,14 @@ _data_files = dict(
 # Class variables - Hyperparameters
 _hyperparameters = dict(
     datasets=dict(
-        # methylation=_data_files["meth_csv_file"],
-        # transcriptomics=_data_files["gexp_csv_file"],
+        methylation=_data_files["meth_csv_file"],
+        transcriptomics=_data_files["gexp_csv_file"],
         proteomics=_data_files["prot_csv_file"],
         metabolomics=_data_files["meta_csv_file"],
         drugresponse=_data_files["dres_csv_file"],
-        # crisprcas9=_data_files["cris_csv_file"],
+        crisprcas9=_data_files["cris_csv_file"],
     ),
-    num_epochs=25,
+    num_epochs=50,
     learning_rate=1e-4,
     batch_size=32,
     n_folds=3,
