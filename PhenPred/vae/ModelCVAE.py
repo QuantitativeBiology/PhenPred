e2038d7f57153cc33b81e1a411d3e69b56947968
Emanuel Gonçalves <emanuelvgoncalves@gmail.com>
2023-05-04 19:28:03 +0100

    gpu paralel

--- PhenPred/vae/ModelCVAE.py
+++ PhenPred/vae/ModelCVAE.py
@@ -6,7 +6,7 @@ import torch.nn as nn
 import torch.nn.functional as F
 
 
-class CLinesCVAE(nn.DataParallel):
+class CLinesCVAE(nn.Module):
     def __init__(self, views, hyper, condi=None):
         super(CLinesCVAE, self).__init__()
 
