f843fc45698f8f39d75c35ff28f9e7c6bad0b3f0
EmanuelGoncalves <emanuelvgoncalves@gmail.com>
2023-05-29 15:30:01 +0100

    MMD to remove batch effects from latent space

--- PhenPred/vae/Losses.py
+++ PhenPred/vae/Losses.py
@@ -8,41 +8,16 @@ import matplotlib.pyplot as plt
 import torch.nn as nn
 import scipy.stats as stats
 import torch.nn.functional as F
+from sklearn import metrics
 from datetime import datetime
-from PhenPred import PALETTE_TTYPE
 from PhenPred import PALETTE_TTYPE
 from sklearn.model_selection import KFold
 from matplotlib.ticker import MaxNLocator
-from PhenPred.vae.PlotUtils import GIPlot
 from PhenPred.vae.PlotUtils import GIPlot
 from PhenPred.vae import data_folder, plot_folder
 
 
 class CLinesLosses:
-    @classmethod
-    def activation_function(cls, name):
-        if name == "relu":
-            return nn.ReLU()
-        elif name == "leaky_relu":
-            return nn.LeakyReLU()
-        elif name == "sigmoid":
-            return nn.Sigmoid()
-        elif name == "tanh":
-            return nn.Tanh()
-        else:
-            return nn.Identity()
-
-    @classmethod
-    def reconstruction_loss(cls, name):
-        if name == "mse":
-            return F.mse_loss
-        elif name == "bce":
-            return F.binary_cross_entropy
-        elif name == "gauss":
-            return nn.GaussianNLLLoss
-        else:
-            return F.mse_loss
-
     @classmethod
     def loss_function(
         cls,
@@ -51,19 +26,14 @@ class CLinesLosses:
         views_hat,
         means,
         log_variances,
+        z_joint,
         views_nans=None,
         covariates=None,
     ):
-        if covariates is not None:
-            covariates_n = covariates.shape[1]
-
         # Compute reconstruction loss across views
         mse_loss, view_mse_loss = 0, {}
         for i, k in enumerate(hypers["datasets"]):
             X, X_ = views[i], views_hat[i]
-
-            if covariates_n != 0:
-                X_ = X_[:, :-covariates_n]
 
             if views_nans is not None:
                 X, X_ = X[views_nans[i]], X_[views_nans[i]]
@@ -85,21 +55,7 @@ class CLinesLosses:
         kl_loss *= hypers["beta"]
 
         # Compute batch covariate loss
-        covariate_loss, view_covariate_loss = 0, {}
-        if covariates_n != 0:
-            for i, k in enumerate(hypers["datasets"]):
-                X, X_ = covariates, views_hat[i][:, -covariates_n:]
-
-                # calculate cross_entropy loss between two matrices
-                v_covariate_loss = torch.mean(
-                    torch.stack(
-                        [F.cross_entropy(X_, X[:, i]) for i in range(X_.shape[1])]
-                    )
-                )
-
-                covariate_loss += v_covariate_loss
-                view_covariate_loss[k] = v_covariate_loss
-            covariate_loss /= len(hypers["datasets"])
+        covariate_loss = 0 if covariates is None else cls.mmd_loss(z_joint, covariates)
 
         # Compute total loss
         total_loss = kl_loss + mse_loss + covariate_loss
@@ -111,8 +67,60 @@ class CLinesLosses:
             kl=kl_loss,
             covariate=covariate_loss,
             mse_views=view_mse_loss,
-            covariate_views=view_covariate_loss,
         )
+
+    @classmethod
+    def mmd_loss(cls, means, covariates):
+        dist = torch.cdist(means, means, p=2)
+
+        covariate_losses = []
+        for i in range(covariates.shape[1]):
+            idx_x = torch.nonzero(covariates[:, i]).flatten()
+            idx_y = torch.nonzero(covariates[:, i] == 0).flatten()
+
+            if len(idx_x) == 0 or len(idx_y) == 0:
+                covariate_losses.append(torch.tensor(0.0))
+                continue
+
+            k_xx = cls.gaussian_kernel(dist[idx_x][:, idx_x]).mean()
+            k_yy = cls.gaussian_kernel(dist[idx_y][:, idx_y]).mean()
+            k_xy = cls.gaussian_kernel(dist[idx_x][:, idx_y]).mean()
+
+            covariate_losses.append(k_xx + k_yy - 2 * k_xy)
+
+        return torch.stack(covariate_losses).mean()
+
+    @classmethod
+    def gaussian_kernel(cls, D, gamma=None):
+        if gamma is None:
+            gamma = [
+                1e-6,
+                1e-5,
+                1e-4,
+                1e-3,
+                1e-2,
+                1e-1,
+                1,
+                5,
+                10,
+                15,
+                20,
+                25,
+                30,
+                35,
+                100,
+                1e3,
+                1e4,
+                1e5,
+                1e6,
+            ]
+
+        K = torch.zeros_like(D)
+
+        for g in gamma:
+            K.add_(torch.exp(D.mul(-g)))
+
+        return K / len(gamma)
 
     @classmethod
     def get_optimizer(cls, hyper, model):
@@ -355,3 +363,33 @@ class CLinesLosses:
                         bbox_inches="tight",
                     )
                     plt.close()
+
+    @classmethod
+    def activation_function(cls, name):
+        if name == "relu":
+            return nn.ReLU()
+        elif name == "leaky_relu":
+            return nn.LeakyReLU()
+        elif name == "sigmoid":
+            return nn.Sigmoid()
+        elif name == "tanh":
+            return nn.Tanh()
+        elif name == "elu":
+            return nn.ELU()
+        elif name == "selu":
+            return nn.SELU()
+        elif name == "softplus":
+            return nn.Softplus()
+        else:
+            return nn.Identity()
+
+    @classmethod
+    def reconstruction_loss(cls, name):
+        if name == "mse":
+            return F.mse_loss
+        elif name == "bce":
+            return F.binary_cross_entropy
+        elif name == "gauss":
+            return nn.GaussianNLLLoss
+        else:
+            return F.mse_loss
