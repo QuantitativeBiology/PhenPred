8b6c5048e0c5fc271c40f8ac73656578e75f93a9
Emanuel Gonçalves <emanuelvgoncalves@gmail.com>
2023-06-24 08:11:06 +0100

    paralleziation args change
    Former-commit-id: 2f2993b77eae40f120cfc52ebd3bdf815dbf3898

--- PhenPred/vae/HypersOptimization.py
+++ PhenPred/vae/HypersOptimization.py
@@ -101,7 +101,7 @@ if __name__ == "__main__":
         OptunaOptimization(clines_db, hyperparameters),
         n_trials=100,
         show_progress_bar=True,
-        n_jobs=32,
+        n_jobs=40,
     )
 
     # Print results
