3dbfb6b48c601b28cebd01a37e91d13915c55fbf
Emanuel Gonçalves <emanuelvgoncalves@gmail.com>
2023-07-03 09:02:35 +0100

    min obs CRISPR changed

--- PhenPred/vae/Model.py
+++ PhenPred/vae/Model.py
@@ -17,7 +17,8 @@ class MOVE(nn.Module):
         self.hypers = hypers
         self.views_sizes = views_sizes
         self.views_latent_sizes = {
-            k: int(v * self.hypers["view_latent_dim"]) for k, v in self.views_sizes.items()
+            k: int(v * self.hypers["view_latent_dim"])
+            for k, v in self.views_sizes.items()
         }
         self.conditional_size = conditional_size
 
@@ -37,7 +38,7 @@ class MOVE(nn.Module):
         latent_views_sum = sum(self.views_latent_sizes.values())
         self.joint = Gaussian(
             latent_views_sum,
-            self.hypers["latent_dim"],
+            self.hypers["view_latent_dim"],
         )
 
         self._build_decoders()
@@ -66,7 +67,7 @@ class MOVE(nn.Module):
             self.encoders.append(nn.Sequential(*layers))
 
     def _build_decoders(self):
-        latent_views_sum = self.hypers["latent_dim"]
+        latent_views_sum = self.hypers["view_latent_dim"]
 
         self.decoders = nn.ModuleList()
         for n in self.views_sizes:
