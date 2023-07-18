cb7a246bbeb046a6f1d307318dc50ef28d97ae5c
Emanuel Gonçalves <emanuelvgoncalves@gmail.com>
2023-07-18 23:07:05 +0100

    hypers updated with optuna runs
    Former-commit-id: adfbdd97913aa24ce17bbdb841a5ce7fcbc099cc

--- PhenPred/vae/BenchmarkMOFA.r
+++ PhenPred/vae/BenchmarkMOFA.r
@@ -1,7 +1,12 @@
 library(MOFA2)
 
+## ---------- Number of factors
+n_factors <- 73
+
 ## ---------- Model, Data e Info Cell Lines
-model <- load_model("data/clines/mofa/MOFA_depmap23Q2_Factors50.hdf5")
+
+# concat names path with n_factors
+model <- load_model(paste0("data/clines/mofa/MOFA_depmap23Q2_Factors", n_factors, ".hdf5"))
 data <- get_data(model, as.data.frame = T)
 head(data, n=3)
 
@@ -10,18 +15,20 @@ data_imputed <- impute(model, views = "all", factors = "all")
 data_predicted <- predict(model, views = "all", factors = "all")
 
 ## ---------- Write imputed / predicted data
-write.csv(data_imputed@imputed_data$crisprcas9$groupA, "data/clines/mofa/MOFA_depmap23Q2_Factors50_imputed_crisprcas9.csv", row.names=TRUE)
-write.csv(data_imputed@imputed_data$drugresponse$groupA, "data/clines/mofa/MOFA_depmap23Q2_Factors50_imputed_drugresponse.csv", row.names=TRUE)
-write.csv(data_imputed@imputed_data$metabolomics$groupA, "data/clines/mofa/MOFA_depmap23Q2_Factors50_imputed_metabolomics.csv", row.names=TRUE)
-write.csv(data_imputed@imputed_data$methylation$groupA, "data/clines/mofa/MOFA_depmap23Q2_Factors50_imputed_methylation.csv", row.names=TRUE)
-write.csv(data_imputed@imputed_data$proteomics$groupA, "data/clines/mofa/MOFA_depmap23Q2_Factors50_imputed_proteomics.csv", row.names=TRUE)
-write.csv(data_imputed@imputed_data$transcriptomics$groupA, "data/clines/mofa/MOFA_depmap23Q2_Factors50_imputed_transcriptomics.csv", row.names=TRUE)
-write.csv(data_imputed@imputed_data$copynumber$groupA, "data/clines/mofa/MOFA_depmap23Q2_Factors50_imputed_copynumber.csv", row.names=TRUE)
+dpath_prefix <- "data/clines/mofa/MOFA_depmap23Q2_Factors"
 
-write.csv(data_predicted$crisprcas9$groupA, "data/clines/mofa/MOFA_depmap23Q2_Factors50_predicted_crisprcas9.csv", row.names=TRUE)
-write.csv(data_predicted$drugresponse$groupA, "data/clines/mofa/MOFA_depmap23Q2_Factors50_predicted_drugresponse.csv", row.names=TRUE)
-write.csv(data_predicted$metabolomics$groupA, "data/clines/mofa/MOFA_depmap23Q2_Factors50_predicted_metabolomics.csv", row.names=TRUE)
-write.csv(data_predicted$methylation$groupA, "data/clines/mofa/MOFA_depmap23Q2_Factors50_predicted_methylation.csv", row.names=TRUE)
-write.csv(data_predicted$proteomics$groupA, "data/clines/mofa/MOFA_depmap23Q2_Factors50_predicted_proteomics.csv", row.names=TRUE)
-write.csv(data_predicted$transcriptomics$groupA, "data/clines/mofa/MOFA_depmap23Q2_Factors50_predicted_transcriptomics.csv", row.names=TRUE)
-write.csv(data_predicted$copynumber$groupA, "data/clines/mofa/MOFA_depmap23Q2_Factors50_predicted_copynumber.csv", row.names=TRUE)
+write.csv(data_imputed@imputed_data$crisprcas9$groupA, paste0(dpath_prefix, n_factors, "_imputed_crisprcas9.csv"), row.names=TRUE)
+write.csv(data_imputed@imputed_data$drugresponse$groupA, paste0(dpath_prefix, n_factors, "_imputed_drugresponse.csv"), row.names=TRUE)
+write.csv(data_imputed@imputed_data$metabolomics$groupA, paste0(dpath_prefix, n_factors, "_imputed_metabolomics.csv"), row.names=TRUE)
+write.csv(data_imputed@imputed_data$methylation$groupA, paste0(dpath_prefix, n_factors, "_imputed_methylation.csv"), row.names=TRUE)
+write.csv(data_imputed@imputed_data$proteomics$groupA, paste0(dpath_prefix, n_factors, "_imputed_proteomics.csv"), row.names=TRUE)
+write.csv(data_imputed@imputed_data$transcriptomics$groupA, paste0(dpath_prefix, n_factors, "_imputed_transcriptomics.csv"), row.names=TRUE)
+write.csv(data_imputed@imputed_data$copynumber$groupA, paste0(dpath_prefix, n_factors, "_imputed_copynumber.csv"), row.names=TRUE)
+
+write.csv(data_predicted$crisprcas9$groupA, paste0(dpath_prefix, n_factors, "_predicted_crisprcas9.csv"), row.names=TRUE)
+write.csv(data_predicted$drugresponse$groupA, paste0(dpath_prefix, n_factors, "_predicted_drugresponse.csv"), row.names=TRUE)
+write.csv(data_predicted$metabolomics$groupA, paste0(dpath_prefix, n_factors, "_predicted_metabolomics.csv"), row.names=TRUE)
+write.csv(data_predicted$methylation$groupA, paste0(dpath_prefix, n_factors, "_predicted_methylation.csv"), row.names=TRUE)
+write.csv(data_predicted$proteomics$groupA, paste0(dpath_prefix, n_factors, "_predicted_proteomics.csv"), row.names=TRUE)
+write.csv(data_predicted$transcriptomics$groupA, paste0(dpath_prefix, n_factors, "_predicted_transcriptomics.csv"), row.names=TRUE)
+write.csv(data_predicted$copynumber$groupA, paste0(dpath_prefix, n_factors, "_predicted_copynumber.csv"), row.names=TRUE)
