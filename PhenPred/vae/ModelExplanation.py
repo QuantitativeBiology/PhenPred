5c701b0f6f0aa299484ebe105ea25524d0dfddeb
Olimon660 <zhaoxiang.cai@hotmail.com>
2023-11-09 16:45:20 +1100

    changes for calculating all SHAP
    Former-commit-id: e4537553b0cbbf649dd250c868a24f1b91e8bbce

--- PhenPred/vae/ModelExplanation.py
+++ PhenPred/vae/ModelExplanation.py
@@ -3,6 +3,7 @@
 
 import os
 import sys
+import time
 
 proj_dir = "/home/scai/PhenPred"
 if not os.path.exists(proj_dir):
@@ -20,7 +21,7 @@ from PhenPred.vae.DatasetDepMap23Q2 import CLinesDatasetDepMap23Q2
 import shap
 
 if __name__ == "__main__":
-    hyperparameters = Hypers.read_hyperparameters(timestamp="20231005_010815")
+    hyperparameters = Hypers.read_hyperparameters(timestamp="20231023_092657")
 
     # Load the first dataset
     clines_db = CLinesDatasetDepMap23Q2(
@@ -39,6 +40,22 @@ if __name__ == "__main__":
         stratify_cv_by=clines_db.samples_by_tissue("Haematopoietic and Lymphoid"),
         timestamp=hyperparameters["load_run"]
     )
+
+
+    start_time = time.time()
+
     train.load_model()
-    train.run_shap(explain_target="drug_response")
-    train.run_shap(explain_target="latent")
+
+    # train.run_shap(explain_target="latent")
+    # train.run_shap(explain_target="drugresponse")
+    
+    # train.run_shap(explain_target="metabolomics")
+    # train.run_shap(explain_target="copynumber")
+    train.run_shap(explain_target="proteomics")
+    train.run_shap(explain_target="crisprcas9")
+    train.run_shap(explain_target="transcriptomics")
+    train.run_shap(explain_target="methylation")
+
+    end_time = time.time()
+    runtime = end_time - start_time
+    print("Runtime: {:02d}:{:02d}:{:02d}".format(int(runtime // 3600), int((runtime % 3600) // 60), int(runtime % 60)))
