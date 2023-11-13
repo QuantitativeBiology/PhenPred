032e1120cc3c5513d319dc774f889aad27457a44
Olimon660 <zhaoxiang.cai@hotmail.com>
2023-11-13 14:41:53 +1100

    Add missing path
    Former-commit-id: 3f6435e75aa7047d0e2717a22cc58220befa80f4

--- PhenPred/vae/__init__.py
+++ PhenPred/vae/__init__.py
@@ -12,6 +12,11 @@ plot_folder = "/home/egoncalves/PhenPred/reports/vae/"
 if not os.path.exists(plot_folder):
     plot_folder = "reports/vae/"
 
+# SHAP dir path - on mounted drive
+shap_folder = "/home/scai/projects/E0160_P01_PhenPred/reports/vae/"
+if not os.path.exists(shap_folder):
+    shap_folder = plot_folder
+
 # Files folder path - INESC-ID server
 files_folder = "/home/egoncalves/PhenPred/data/"
 
