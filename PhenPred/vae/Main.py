49be2f5ffd48d76e0636eb3a7f498b0365b71669
Emanuel Gonçalves <emanuelvgoncalves@gmail.com>
2024-01-10 11:08:39 +0000

    transcriptomics update
    Former-commit-id: 17c3cb2ed5aa956db3202c46c35d70f21e19ea93

--- PhenPred/vae/Main.py
+++ PhenPred/vae/Main.py
@@ -4,29 +4,32 @@
 import os
 import sys
 
+from sqlalchemy import column
+
 proj_dir = os.getcwd()
 if proj_dir not in sys.path:
     sys.path.append(proj_dir)
 
 import json
 import torch
-import numpy as np
 import PhenPred
 import argparse
+import numpy as np
 import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
-from PhenPred.vae import plot_folder
 from PhenPred.vae.Hypers import Hypers
-from PhenPred.vae.Train import CLinesTrain
 from sklearn.model_selection import KFold
+from PhenPred.vae.Train import CLinesTrain
+from PhenPred.Utils import two_vars_correlation
+from PhenPred.vae import plot_folder, data_folder
 from PhenPred.vae.DatasetMOFA import CLinesDatasetMOFA
-from PhenPred.vae.DatasetDepMap23Q2 import CLinesDatasetDepMap23Q2
 from PhenPred.vae.BenchmarkCRISPR import CRISPRBenchmark
 from PhenPred.vae.BenchmarkDrug import DrugResponseBenchmark
 from PhenPred.vae.BenchmarkMismatch import MismatchBenchmark
 from PhenPred.vae.BenchmarkProteomics import ProteomicsBenchmark
 from PhenPred.vae.BenchmarkLatentSpace import LatentSpaceBenchmark
+from PhenPred.vae.DatasetDepMap23Q2 import CLinesDatasetDepMap23Q2
 
 
 torch.manual_seed(0)
@@ -36,7 +39,7 @@ if __name__ == "__main__":
     # Class variables - Hyperparameters
 
     hyperparameters = Hypers.read_hyperparameters()
-    # hyperparameters = Hypers.read_hyperparameters(timestamp="20240104_121846")
+    # hyperparameters = Hypers.read_hyperparameters(timestamp="20231023_092657")
 
     # Load the first dataset
     clines_db = CLinesDatasetDepMap23Q2(
@@ -63,6 +66,64 @@ if __name__ == "__main__":
     vae_predicted, _ = train.load_vae_reconstructions(mode="all")
 
     mofa_imputed, mofa_latent = CLinesDatasetMOFA.load_reconstructions(clines_db)
+
+    # Transcriptomics benchmark
+    samples_mgexp = ~clines_db.dfs["transcriptomics"].isnull().all(axis=1)
+
+    gexp_gdsc = pd.read_csv(f"{data_folder}/transcriptomics.csv", index_col=0).T
+    gexp_move = vae_imputed["transcriptomics"]
+
+    samples = set(gexp_gdsc.index).intersection(gexp_move.index)
+    genes = list(set(gexp_gdsc.columns).intersection(gexp_move.columns))
+
+    gexp_corr = pd.DataFrame(
+        [
+            two_vars_correlation(
+                gexp_gdsc.loc[s, genes],
+                gexp_move.loc[s, genes],
+                method="pearson",
+                extra_fields=dict(sample=s, with_gexp=samples_mgexp.loc[s]),
+            )
+            for s in samples
+        ]
+    )
+
+    _, ax = plt.subplots(1, 1, figsize=(2, 0.75), dpi=600)
+
+    sns.boxplot(
+        data=gexp_corr,
+        x="corr",
+        y="with_gexp",
+        orient="h",
+        palette="tab20c",
+        linewidth=0.3,
+        fliersize=1,
+        notch=True,
+        saturation=1.0,
+        showcaps=False,
+        boxprops=dict(linewidth=0.5, edgecolor="black"),
+        whiskerprops=dict(linewidth=0.5, color="black"),
+        flierprops=dict(
+            marker="o",
+            markerfacecolor="black",
+            markersize=1.0,
+            linestyle="none",
+            markeredgecolor="none",
+            alpha=0.6,
+        ),
+        medianprops=dict(linestyle="-", linewidth=0.5),
+        ax=ax,
+    )
+
+    ax.set(
+        title=f"",
+        xlabel="Correlation between reconstructed\nand GDSC transcriptomics (Pearson's r)",
+        ylabel="Sample\nwith transcriptomics",
+    )
+
+    PhenPred.save_figure(
+        f"{plot_folder}/{hyperparameters['load_run']}_reconstructed_gexp_correlation_boxplot"
+    )
 
     # Run Latent Spaces Benchmark
     latent_benchmark = LatentSpaceBenchmark(
