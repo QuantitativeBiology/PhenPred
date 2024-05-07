a501ceb57b5393351abbf485e54111beb5e863e5
Olimon660 <zhaoxiang.cai@hotmail.com>
2024-05-07 14:25:31 +1000

    fix path for shap
    Former-commit-id: eabcd56f56ccb01f22249f3e3056054e60f697b2

--- PhenPred/vae/__init__.py
+++ PhenPred/vae/__init__.py
@@ -9,8 +9,4 @@ if proj_dir not in sys.path:
 files_folder = f"{proj_dir}/data/"
 data_folder = f"{proj_dir}/data/clines/"
 plot_folder = f"{proj_dir}/reports/vae/"
-
-# SHAP dir path - on mounted drive
-shap_folder = "/home/scai/projects/E0160_P01_PhenPred/reports/vae/"
-if not os.path.exists(shap_folder):
-    shap_folder = plot_folder
+shap_folder = plot_folder
